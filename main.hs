1f3c4a0f0bdee99fd51fe2417dfad50615e47ee6
logiclogue <jordanpaullord@hotmail.co.uk>
2019-03-06 08:46:54 +0000

    Adds native function support

--- main.hs
+++ main.hs
@@ -7,6 +7,7 @@ data TypeExpr = Func TypeExpr TypeExpr
 data Expr = Lambda String Expr
           | App Expr Expr
           | Symbol String
+          | NativeFunction (Expr -> Expr)
           | N Int
 
 instance Show Expr where
@@ -14,17 +15,24 @@ instance Show Expr where
     show (Lambda s expr) = "(lambda (" ++ s ++ ") " ++ (show expr) ++ ")"
     show (App expr_a expr_b) = "(" ++ (show expr_a) ++ " " ++ (show expr_b) ++ ")"
     show (Symbol s) = s
+    show (NativeFunction _) = "<native function>"
     show (N n) = show n
 
 eval_expr :: Expr -> (String -> Expr) -> Expr
 eval_expr (App (Lambda var expr_a) expr_b) env =
     eval_expr expr_a (\x -> if x == var then expr_b else env x)
 
+eval_expr (App (NativeFunction f) expr) env =
+    eval_expr (f (eval_expr expr env)) env
+
 eval_expr (App expr_a expr_b) env =
     eval_expr (App (eval_expr expr_a env) expr_b) env
 
 eval_expr (Lambda var expr_a) env =
     Lambda var expr_a
+
+eval_expr (NativeFunction f) env =
+    NativeFunction f
 
 eval_expr (Symbol s) env =
     eval_expr (env s) env
@@ -34,6 +42,7 @@ eval_expr (N n) env =
 
 my_env :: String -> Expr
 my_env "jordan" = (Lambda "x" (Symbol "x"))
+my_env "succ" = (NativeFunction (\(N x) -> N (x + 1)))
 
 identifier :: Parser String
 identifier = some (satisfy isAlpha)
