d32f3d5b7df1d2c618aace8f0b5727364a6d384b
logiclogue <jordanpaullord@hotmail.co.uk>
2019-03-15 22:41:53 +0000

    Adds monotypes and polytypes

--- Types.hs
+++ Types.hs
@@ -1,5 +1,13 @@
 module Types where
 
+data MonoType
+    = MonoTypeVar String
+    | MonoTypeApp MonoType MonoType
+
+data PolyType
+    = PolyTypeMono MonoType
+    | PolyTypeForAll String PolyType
+
 data Type
     = Number
     | Function (Type, Type)
