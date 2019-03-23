4365fc42eb2a03d0e9dc37777699681306754649
logiclogue <jordanpaullord@hotmail.co.uk>
2019-03-23 22:59:33 +0000

    Removes old types

--- Types.hs
+++ Types.hs
@@ -13,23 +13,3 @@ data TypeContext
     | TypeContextAssign String PolyType
 
 data Typing = Typing Expr PolyType
-
-data Type
-    = Number
-    | Function (Type, Type)
-    | ForAll String Type
-    | TypeSymbol String
-
-instance Show Type where
-
-    show Number =
-        "Number"
-
-    show (Function (t1, t2)) =
-        show t1 ++ " -> " ++ show t2
-
-    show (ForAll var t) =
-        "forall " ++ var ++ ". " ++ show t
-
-    show (TypeSymbol var) =
-        var
