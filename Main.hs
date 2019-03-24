e59cead85ef5b083e643913beb18038476599bbf
logiclogue <jordanpaullord@hotmail.co.uk>
2019-03-24 09:31:26 +0000

    Adds types for prelude functions, don't know if it passes

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 import Expr (Expr (..))
 import Evaluator (eval_expr)
 import Parser (parse_expr)
+import Types
 
 data TypeExpr = Func TypeExpr TypeExpr
               | TypeInt Int
@@ -9,6 +10,24 @@ my_prelude :: String -> Expr
 my_prelude "jordan" = (Lambda "x" (Symbol "x"))
 my_prelude "succ" = (NativeFunction (\(N x) -> N (x + 1)))
 my_prelude "add" = (NativeFunction (\(N x) -> NativeFunction (\(N y) -> N (x + y))))
+
+type_prelude :: [TypeContext]
+type_prelude =
+    [
+        TypeContextAssign "jordan" (PolyTypeForAll "a"
+            (PolyTypeMono (MonoTypeApp (MonoTypeApp
+                (MonoTypeVar "->") (MonoTypeVar "a")) MonoTypeVar "a")),
+        TypeContextAssign "succ" (PolyTypeMono (MonoTypeApp (MonoTypeApp
+            (MonoTypeVar "->") (MonoTypeVar "Number")) (MonoTypeVar "Number"))),
+        TypeContextAssign "add" (PolyTypeMono
+            (MonoTypeApp
+                (MonoTypeApp
+                    (MonoTypeVar "->")
+                    (MonoTypeApp
+                        (MonoTypeApp (MonoTypeVar "->") (MonoTypeVar "Number"))
+                        (MonoTypeVar "Number")))
+                (MonoTypeVar "Number")))
+    ]
 
 main :: IO ()
 main = do
